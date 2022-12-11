4027c8d81027ce1853c3c5e323f8f3dff6f2254e
ktakeda <ktakeda@st.hsc.ac.jp>
2022-12-11 21:59:50 +0900

    自作関数と無名関数の確認

--- nikkei_software.py
+++ nikkei_software.py
@@ -1,17 +1,19 @@
-#組み込み関数　インポートせずに利用可能な関数
-data = [1,2,3,4]
-print(len(data)) #print関数とlen関数
-
-#ライブラリ関数の活用
-
-#ライブラリ関数の集まりモジュールの呼び出しと利用例
-import random
-print(random.randint(1,6))
-
-#モジュールに別名をつけて利用例
-import random as r
-print(r.randint(1,6))
-
-#モジュールからライブラリ関数を取り出して利用例
+#自作関数の定義
 from random import randint
-print(randint(1,10))
+def normal_dice():
+    return randint(1,6)
+
+#自作関数の呼び出し
+print(normal_dice())
+
+#規定値の活用
+def multi_dice(eye = 6): #規定値6
+    return randint(1,eye)
+
+print(multi_dice(100))
+
+#無名関数（ラムダ式）
+dice = lambda eye=6:randint(1,eye)
+
+print(dice(4))
+
