90b45333bb97349c9efdc5eab4dcdf81b2556fd8
ktakeda <ktakeda@st.hsc.ac.jp>
2022-12-12 21:25:56 +0900

    条件判断の確認

--- nikkei_software.py
+++ nikkei_software.py
@@ -1,19 +1,8 @@
-#自作関数の定義
-from random import randint
-def normal_dice():
-    return randint(1,6)
+#条件判断
+import random
 
-#自作関数の呼び出し
-print(normal_dice())
-
-#規定値の活用
-def multi_dice(eye = 6): #規定値6
-    return randint(1,eye)
-
-print(multi_dice(100))
-
-#無名関数（ラムダ式）
-dice = lambda eye=6:randint(1,eye)
-
-print(dice(4))
+if random.randint(1,2) == 1:#真
+    print("奇数")
+else:#偽　偽というのはどうだろうか・・・
+    print("偶数")
 
