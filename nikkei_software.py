74eb099d8d77d8d0f20a1c2cc6dd34c786b23559
ktakeda <ktakeda@st.hsc.ac.jp>
2022-12-13 21:58:09 +0900

    ウィジェット（ボタン）をパック確認

--- nikkei_software.py
+++ nikkei_software.py
@@ -1,9 +1,15 @@
 #GUI:tkinter
 
 from tkinter import *
-from tkinter import ttk
+from tkinter import messagebox,ttk
+
+def buttonClicked():
+    messagebox.showinfo(root.title(),"ありがとうございます！")
 
 root = Tk()
 root.title("TextEdit")
 root.geometry("300x50")
+
+ttk.Button(text="押してください",command=buttonClicked).pack()
+
 root.mainloop()
